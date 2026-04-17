362b817b015229031cba05f281829e1aaa9abe4e
oacedamy-droid <oacedamy@gmail.com>
2026-04-18 03:22:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-// Intro animation control
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 4000);
+
+// intro animation handling
 window.addEventListener("load", () => {
-
   const intro = document.getElementById("intro");
+  const body = document.body;
 
-  // After 2.5s → fade out
   setTimeout(() => {
     intro.classList.add("intro-hide");
+    body.classList.remove("intro-lock");
   }, 2500);
 
+  setTimeout(() => {
+    intro.remove();
+  }, 3300);
 });
