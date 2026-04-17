1013649d1ba8ef7662432cabf5ce79534830f643
oacedamy-droid <oacedamy@gmail.com>
2026-04-18 03:01:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const slides = document.querySelectorAll(".slide");
-let index = 0;
-
-setInterval(() => {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
-  slides[index].classList.add("active");
-}, 4000);
+// Fade only once when page loads
+window.addEventListener("load", () => {
+  document.querySelector(".hero-content").classList.add("fade-in");
+});
